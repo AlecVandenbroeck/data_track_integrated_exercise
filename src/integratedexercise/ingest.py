36ef3b56afc2ab5ce4938b4dff9a600eacc88238
Alec Van den broeck <alec.vandenbroeck@ugent.be>
2023-11-23 13:49:20 +0000

    Write basic functionality to create s3 bucket and get API data

--- src/integratedexercise/ingest.py
+++ src/integratedexercise/ingest.py
@@ -4,10 +4,55 @@ from time import sleep
 import argparse
 import requests
 import logging
+from datetime import datetime
+
+from botocore.client import ClientError
+import boto3
+
+CATEGORY_IDS = ["1", "5", "8", "71", "6001"]
+
+def ingest_data(env, date):
+    station_id = 1164
+    endpoint = f'https://geo.irceline.be/sos/api/v1/stations/{station_id}?expanded=true'
+
+    raw_data = requests.get(endpoint).json()
+    time_series = raw_data['properties']['timeseries']
+
+    # only keep timeseries ids which correspond to one of the interesting category ids
+    filtered_tsi = [x for x in time_series.keys() if time_series[x]['category']['id'] in CATEGORY_IDS]
+    
+    for tsi in filtered_tsi:
+        # get the values for a day of all these timeseries
+        raw_timeseries_data = get_timeseries_of_date(tsi, date)
+
+        # push the values to the s3 bucket
+        print(len(raw_timeseries_data['values']))
+        for i in raw_timeseries_data['values']:
+            print(f'{datetime.fromtimestamp(i['timestamp']/1000)}: {i['value']}')
+        pass
+
+
+def get_timeseries_of_date(timeseries_id, date):
+    endpoint = f"https://geo.irceline.be/sos/api/v1/timeseries/{timeseries_id}/getData?timespan=PT24H/{date}"
+    raw_timeseries_data = requests.get(endpoint).json()
+    return raw_timeseries_data
+
+def create_s3_if_not_exists(bucket_name):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket.name)
+    except ClientError:
+        print(f'[LOG] Creating S3 bucket named {bucket_name}')
+        bucket = s3.create_bucket(Bucket=bucket_name)
+
+    return bucket
+
+def push_s3(raw_data):
+    pass
 
 def process_raw_data(s3_bucket: str, date: str):
-    station_id = 1163
-    raw_data = requests.get('https://geo.irceline.be/sos/api/v1/stations/{station_id}?expanded=true')
     pass
 
 def main():
@@ -21,7 +66,9 @@ def main():
     )
     args = parser.parse_args()
     logging.info(f"Using args: {args}")
-    ingest_data(args.path, args.date)
+
+    create_s3_if_not_exists('irceline-data')
+    ingest_data(args.env, args.date)
 
 if __name__ == "__main__":
     main()
